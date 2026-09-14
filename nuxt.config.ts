dd605adb7c2dbcab2352ee5d9d1913cd395a6409
Ray Liu <rray12tw346@gmail.com>
2026-09-14 22:27:05 +0800

    feat: add journal PWA icon assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,8 +56,8 @@ export default defineNuxtConfig({
       background_color: '#F4EDE0',
       theme_color: '#F4EDE0',
       icons: [
-        { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-        { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
+        { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+        { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
       ],
     },
     workbox: {
